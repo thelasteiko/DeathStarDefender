96c58fd6393a431d043dc72e5c35c54cf57cf1b9
Julia Behnen <jmbehnen@gmail.com>
2016-01-29 15:07:48 -0800

    Row and column system now, have go pieces placed in center of grid squares (sort of). Updated unit code.

--- main/levelscene.js
+++ main/levelscene.js
@@ -18,15 +18,7 @@ GameBoard.prototype.constructor = GameBoard;
 
 GameBoard.prototype.update = function () {
     if (this.game.click) {
-        this.board[this.game.click.x][this.game.click.y] = this.player;
-        this.player = this.player === 1 ? 2 : 1;
-    }
-    Entity.prototype.update.call(this);
-}
-
-GameBoard.prototype.update = function () {
-    if (this.game.click) {
-        this.board[this.game.click.x][this.game.click.y] = this.player;
+        this.board[this.game.click.col][this.game.click.row] = this.player;
         this.player = this.player === 1 ? 2 : 1;
     }
     Entity.prototype.update.call(this);
@@ -36,7 +28,7 @@ GameBoard.prototype.draw = function (ctx) {
     ctx.drawImage(ASSET_MANAGER.getAsset("./main/img/960px-Blank_Go_board.png"), this.x, this.y, 760, 760);
 
     var size = 39.55;
-    var offset = 3.5 + size/2;
+    var offset = 0; //3.5 + size/2;
 
     for (var i = 0; i < 19; i++) {
         for (var j = 0; j < 19; j++) {
@@ -56,8 +48,8 @@ GameBoard.prototype.draw = function (ctx) {
     if (this.game.mouse) {
         ctx.save();
         ctx.globalAlpha = 0.5;
-        if(this.player === 1) ctx.drawImage(ASSET_MANAGER.getAsset("./main/img/black.png"), this.game.mouse.x * size + offset, this.game.mouse.y * size + offset, 40, 40);
-        else ctx.drawImage(ASSET_MANAGER.getAsset("./main/img/white.png"), this.game.mouse.x * size + offset, this.game.mouse.y * size + offset, 40, 40);
+        if(this.player === 1) ctx.drawImage(ASSET_MANAGER.getAsset("./main/img/black.png"), this.game.mouse.col * size + offset, this.game.mouse.row * size + offset, 40, 40);
+        else ctx.drawImage(ASSET_MANAGER.getAsset("./main/img/white.png"), this.game.mouse.col * size + offset, this.game.mouse.row * size + offset, 40, 40);
         ctx.restore();
     }
 }
@@ -88,29 +80,34 @@ LevelScene.prototype.init = function () {
     this.addEntity(new GameBoard(this.game));
 }
 
+LevelScene.prototype.getRowAndCol = function (x, y) {
+    col = x - this.game.ctx.canvas.getBoundingClientRect().left - 23.5;
+    row = y - this.game.ctx.canvas.getBoundingClientRect().top - 23.5;
+
+    col = Math.floor(x / 39.55);
+    row = Math.floor(y / 39.55);
+
+    // console.log({ row: row, col: col });
+    return { row: row, col: col };
+}
+
 LevelScene.prototype.startInput = function () {
     console.log('Starting input');
     var that = this;
 
-    var getXandY = function (e) {
-        var x = e.clientX - that.game.ctx.canvas.getBoundingClientRect().left - 23.5;
-        var y = e.clientY - that.game.ctx.canvas.getBoundingClientRect().top - 23.5;
-
-        x = Math.floor(x / 39.55);
-        y = Math.floor(y / 39.55);
-
-        return { x: x, y: y };
-    }
-
     this.game.ctx.canvas.addEventListener("mousemove", function (e) {
         //console.log(getXandY(e));
-        that.game.mouse = getXandY(e);
+        that.game.mouse = that.getRowAndCol(e.clientX, e.clientY);
     }, false);
 
     this.game.ctx.canvas.addEventListener("click", function (e) {
         //console.log(getXandY(e));
-        that.game.click = getXandY(e);
+        that.game.click = that.getRowAndCol(e.clientX, e.clientY);
     }, false);
 
     console.log('Input started');
+}
+
+LevelScene.prototype.fireProjectile = function(projectile) {
+
 }
--- main/unitentities.js
+++ main/unitentities.js
@@ -26,20 +26,45 @@ Sun.prototype.constructor = Sun;
 
 // ALLIES
 
-function Ally(game, idleAnim, attackAnim, x, y, projectile, hp) {
+function Ally(game, idleAnim, attackAnim, attackCallback, x, y, hp, projectile, projectileAp) {
     this.attacking = false;
-    this.idleAnimation = idleAnim;
-    this.attackAnimation = attackAnim;
-    this.projectile = projectile; // is the constructor for a new projectile of the right type
+    this.idleAnim = idleAnim;
+    this.attackAnim = attackAnim;
+    this.attackCallback = attackCallback;
+    this.projectile = projectile; // projectile object of the right type
     Unit.call(game, x, y, hp, 0);
 }
 
 Ally.prototype = new Unit();
 Ally.prototype.constructor = Ally;
 
+Ally.prototype.update = function () {
+    if (this.attacking) { // attacking
+        if (this.attackAnim.isDone()) { // currently launching projectile at end of attack animation
+            this.attackAnim.elapsedTime = 0;
+            this.attacking = false;
+            this.fireProjectile();
+        }
+    } else { // approaching from the right
+
+    }
+
+    // Inhertiting classes must call this on their own
+    // Entity.prototype.update.call(this);
+}
+
 Ally.prototype.draw = function (ctx) {
-    // draw a frame of something
+    if (this.attacking) { // attacking
+        this.attackAnim.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    } else { // approaching from the right
+        this.approachAnim.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    }
     Entity.prototype.draw.call(this);
+}
+
+Ally.prototype.fireProjectile = function () {
+    var projectile = this.projectile.clone(this.x, this.y); // launches a new projectile from current ally location
+    this.attackCallback(projectile);
 }
 
 // ENEMIES
@@ -57,23 +82,48 @@ Enemy.prototype = new Unit();
 Enemy.prototype.constructor = Enemy;
 
 Enemy.prototype.update = function () {
+    if (this.waiting) { // waiting between attacks
+
+    } else if (this.attacking) { // attacking
+
+    } else { // approaching from the right
+
+    }
     // something happens here
     Entity.prototype.update.call(this);
 }
 
 Enemy.prototype.draw = function (ctx) {
-    // draw a frame of something
+    if (this.waiting) { // waiting between attacks
+        this.waitAnim.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    } else if (this.attacking) { // attacking
+        this.attackAnim.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    } else { // approaching from the right
+        this.approachAnim.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    }
     Entity.prototype.draw.call(this);
 }
 
 // PROJECTILES
 
 function Projectile(game, animation, x, y, ap) {
+    this.animation = animation;
     Unit.call(game, x, y, 0, ap);
 }
 
 Projectile.prototype = new Unit();
 Projectile.prototype.constructor = Projectile;
+
+// Clones, with the option to set a new location
+Projectile.prototype.clone = function(optX, optY) {
+    var newProjectile = new Projectile(this.game, this.animation, this.x, this.y, this.ap);
+    if (optX && optY) {
+        this.x = optX;
+        this.y = optY;
+    }
+    newProjectile.removeFromWorld = this.removeFromWorld;
+    return newProjectile;
+}
 
 Projectile.prototype.draw = function (ctx) {
     this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
