9c04598fca45195188cfb05d3c1bedf7a906cd39
Julia Behnen <jmbehnen@gmail.com>
2016-01-29 02:32:20 -0800

    Basic unit class structure slightly less silly now.

--- main/unitentities.js
+++ main/unitentities.js
@@ -1,23 +1,11 @@
-function Unit(game, animation, x, y, hp, ap) {
+function Unit(game, x, y, hp, ap) {
     this.hp = hp; // health points
     this.ap = ap; // attack points
-    this.animation = animation; // See explanim in Explosion
     Entity.call(this, game, x, y);
 }
 
 Unit.prototype = new Entity();
 Unit.prototype.constructor = Unit;
-
-//this is where I would move the x and y if there is anything to move
-Unit.prototype.update = function () {
-    //calls the entity's update function which is not doing anything right now...
-    Entity.prototype.update.call(this);
-}
-//This is where I would change the animation
-Unit.prototype.draw = function (ctx) {
-    this.animation.drawFrame(this.game.game.clockTick, ctx, this.x, this.y);
-    Entity.prototype.draw.call(this);
-}
 
 // Other is another unit
 Unit.prototype.attack = function(other) {
@@ -27,37 +15,70 @@ Unit.prototype.attack = function(other) {
     }
 }
 
-function Sun(game, animation, x, y) {
-    Unit.call(game, animation, x, y, 0, 0);
+// SUN (may not need this class, just use unit class)
+
+function Sun(game, x, y) {
+    Unit.call(game, x, y, 0, 0);
 }
 
 Sun.prototype = new Unit();
 Sun.prototype.constructor = Sun;
 
-function Ally(game, animation, x, y, projectile, hp) {
+// ALLIES
+
+function Ally(game, idleAnim, attackAnim, x, y, projectile, hp) {
     this.attacking = false;
+    this.idleAnimation = idleAnim;
+    this.attackAnimation = attackAnim;
     this.projectile = projectile; // is the constructor for a new projectile of the right type
-    Unit.call(game, animation, x, y, hp, 0);
+    Unit.call(game, x, y, hp, 0);
 }
 
 Ally.prototype = new Unit();
 Ally.prototype.constructor = Ally;
 
-function Enemy(game, animation, x, y, hp, ap) {
+Ally.prototype.draw = function (ctx) {
+    // draw a frame of something
+    Entity.prototype.draw.call(this);
+}
+
+// ENEMIES
+
+function Enemy(game, approachAnim, waitAnim, attackAnim, x, y, hp, ap) {
     this.waiting = false;
     this.attacking = false;
-    Unit.call(game, animation, x, y, hp, ap);
+    this.approachAnim = approachAnim;
+    this.waitAnim = waitAnim;
+    this.attackAnim = attackAnim;
+    Unit.call(game, x, y, hp, ap);
 }
 
 Enemy.prototype = new Unit();
 Enemy.prototype.constructor = Enemy;
 
+Enemy.prototype.update = function () {
+    // something happens here
+    Entity.prototype.update.call(this);
+}
+
+Enemy.prototype.draw = function (ctx) {
+    // draw a frame of something
+    Entity.prototype.draw.call(this);
+}
+
+// PROJECTILES
+
 function Projectile(game, animation, x, y, ap) {
-    Unit.call(game, animation, x, y, 0, ap);
+    Unit.call(game, x, y, 0, ap);
 }
 
 Projectile.prototype = new Unit();
 Projectile.prototype.constructor = Projectile;
+
+Projectile.prototype.draw = function (ctx) {
+    this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    Entity.prototype.draw.call(this);
+}
 
 Projectile.prototype.attack = function (other) {
     super.attack(other);
