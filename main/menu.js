dc29e54371915484deae1956ce428b38a3eef4ec
Melinda Robertson <memre911@gmail.com>
2016-02-02 08:51:36 -0800

    Deleted unnecessary code, put Scene in engine.
    * Deleted scene files that were for testing and are no longer needed.
    * Moved the Scene parent class to game engine.
    * Made progress on the menu.

--- main/menu.js
+++ main/menu.js
@@ -1,10 +1,46 @@
+function MenuCounter (game, x, y) {
+    this.energycount = 50; //initial energy
+    this.energyTime = 0;
+    this.textoffset = {x: 26, y: 46};
+    //this.spritesheet = ASSET_MANAGER.getAsset();
+    Entity.prototype.call(this, game, x, y);
+}
+
+MenuCounter.prototype = new Entity();
+MenuCounter.prototype.constructor = MenuCounter;
+
+MenuCounter.prototype.update = function() {
+    this.energyTime += this.game.game.clockTick;
+    if(energyTime >= 10) {
+        this.energycount += 25;
+        this.energyTime = 0;
+    }
+    Entity.prototype.update.call(this);
+}
+
+MenuCounter.prototype.draw = function(ctx) {
+    /*ctx.drawImage(this.spritesheet,
+        this.x, this.y, 64, 64);*/
+    ctx.font="20px Verdana";
+    ctx.fillText(this.energycount,
+        this.x + this.textoffset.x, this.y + this.textoffset.y);
+}
+
+function MenuItem (game, x, y, title, price, animation, availablepic, selectedpic) {
+    Entity.prototype.call(this, game, x, y);
+    this.title = title;
+    this.price = price;
+    this.animation = animation;
+    this.availablepic = availablepic;
+    this.selectedpic = selectedpic;
+}
+
 //items should appear in a row
-//the first item is a counter for energy
-function Menu (game, x, y, minpick) {
-    Entity.prototype.call(game, x, y);
+function Menu (game, x, y) {
+    Entity.prototype.call(this, game, x, y);
     //this.spritesheet = ASSET_MANAGER.getAsset("./main/img");
     this.current = null;
-    this.minpick = minpick;
+    this.counter = new MenuCounter(game, this.x, this.y);
     this.items = [];
 }
 
@@ -12,5 +48,25 @@ Menu.prototype = new Entity();
 Menu.prototype.constructor = Menu;
 
 Menu.prototype.update = function() {
-    //update items in the
+    if(current) {
+        
+    }
+}
+
+Menu.prototype.draw = function() {
+    
+}
+
+Menu.prototype.setSelection = function (x, y) {
+    //bounds x > 64, x < 64 * items.length
+    //bounds y > 0, y < 64
+    if (y > 0 && y < 64) {
+        var i = x % 64;
+    }
+}
+
+Menu.prototype.addItem(game, title, price, animation, availablepic, selectedpic) {
+    var x = (this.items.length+1) * 64;
+    var y = this.y;
+    this.items.push(new MenuItem(game, x, y, title, price, animation, availablepic, selectedpic));
 }
